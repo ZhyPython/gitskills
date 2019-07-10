fefb21d67e16356ffffafd5403b51e2d7b6311e1
hanyu <zhuhanyu1@gmail.com>
2019-07-10 20:27:05 +0800

    next page

--- scrapyspider/spiders/douban_spider.py
+++ scrapyspider/spiders/douban_spider.py
@@ -11,32 +11,37 @@ class DoubanMovieRanking(Spider):
     }
 
     def start_requests(self):
-        url = 'https://movie.douban.com/chart'
+        url = 'https://movie.douban.com/top250'
         print('####################################')
         yield Request(url, headers=self.headers)
 
     def parse(self, response):
-        '''
+        """
         xpath 中的//指在这个HTML中所有的能够匹配的节点，
         .//指在当前目录下所有的能够匹配的节点，
         //a//b指在a节点下所有能够匹配到b（后代节点）
         //a/b指在a节点下能够匹配到的b子节点
-        :param response:
+        :param response
         :return: item
-        '''
+        """
         print('******************************************1')
         item = DoubanMovieItem()
-        movies = response.xpath('//div[@id="content"]//div[@class="indent"]//table')
+        movies = response.xpath('//div[@id="content"]//ol[@class="grid_view"]/li')
         print(movies)
         for movie in movies:
             item['movie_name'] = movie.xpath(
-                './/a[@class=""]/text()'
+                './/span[@class="title"]/text()'
             ).extract()[0].split()[0]
             item['score'] = movie.xpath(
-                './/div[@class="star clearfix"]/span[@class="rating_nums"]/text()'
+                './/div[@class="bd"]/div[@class="star"]/span[@class="rating_num"]/text()'
             ).extract()[0]
             item['score_num'] = movie.xpath(
-                './/div[@class="star clearfix"]/span[@class="pl"]/text()'
+                './/div[@class="bd"]/div[@class="star"]/span[4]/text()'
             ).extract()[0]
             print('******************************************')
             yield item
+
+        next_url = response.xpath('//span[@class="next"]/a/@href').extract()
+        if next_url:
+            next_url = 'https://movie.douban.com/top250' + next_url[0]
+            yield Request(next_url, headers=self.headers)
