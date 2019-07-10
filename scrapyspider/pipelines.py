b1b212b74c1594dd7af25dec4d47faf7aed8131f
hanyu <zhuhanyu1@gmail.com>
2019-07-10 15:12:48 +0800

    save as xlsx

--- scrapyspider/pipelines.py
+++ scrapyspider/pipelines.py
@@ -4,8 +4,17 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from openpyxl import Workbook
 
 
 class ScrapyspiderPipeline(object):
+    def __init__(self):
+        self.wb = Workbook()
+        self.ws = self.wb.active
+        self.ws.append(['电影名称', '分数', '评论人数'])
+
     def process_item(self, item, spider):
+        line = [item['movie_name'], item['score'], item['score_num']]
+        self.ws.append(line)
+        self.wb.save('job.xlsx')
         return item
